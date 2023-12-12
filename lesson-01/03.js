9e0fe2cee86deb9772cd88b4e926fa7365264704
Ruslan_Trafimovich <ruworv@gmail.com>
2023-12-12 22:06:08 +0300

    Урок 1 Задание 3

--- lesson-01/03.js
+++ lesson-01/03.js
@@ -1,5 +1,7 @@
-​let number = 1;
-// console.log(number++); 
-// console.log(++number); 
-// console.log(number++); 
-// console.log(++number); 
+let number = 1;
+console.log(number++);
+number++;
+console.log(number++);
+console.log(--number);
+number--;
+console.log(--number);
